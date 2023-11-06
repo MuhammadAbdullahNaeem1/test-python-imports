0070a8407498acc97cd710c85d8fe7b8822f4b01
Muhammad Abdullah Naeem <abdullahnaeem175@gmail.com>
2023-11-06 10:02:38 +0500

    Update GoogleNewsOperations.py

--- GoogleNewsOperations/GoogleNewsOperations.py
+++ GoogleNewsOperations/GoogleNewsOperations.py
@@ -1,6 +1,20 @@
-from PgOperationsMain.PgOperationsMain import PgOperationsMain
+# from PgOperationsMain.PgOperationsMain import PgOperationsMain
+# class GoogleNewsOperations:
+#     def __init__(self):
+#         pgOperationsMain = PgOperationsMain()
+#         print("GoogleNewsOperations init")
+#         pass
+
+
+import sys
+import os
+
+
+pg_operations_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'PgOperationsMain')
+sys.path.append(pg_operations_path)
+
+from PgOperationsMain import PgOperationsMain
+
 class GoogleNewsOperations:
     def __init__(self):
-        pgOperationsMain = PgOperationsMain()
-        print("GoogleNewsOperations init")
         pass
